28961dcbc4a2fb991849b95ddee35d4c3bdc4ccc
anurag-katariya <anuragkatariya357@gmail.com>
2026-08-31 09:59:05 +0000

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)//we are using nodejs, not browser
+
+console.log(3 + 3); 
+
+let name = "Anurag"
+let age = 25
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power of 53
+//bigint => larger than 2 to power of 53
+//string => "Anurag"(better to use this) or 'Anurag' or `Anurag`
+//boolean => true or false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => unique
+//object 
+
+console.log(typeof null);
+
